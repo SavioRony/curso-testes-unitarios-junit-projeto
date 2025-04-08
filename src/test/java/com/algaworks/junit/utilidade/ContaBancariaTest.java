18375d51c1f172a461a3b6421f7ceec501b6ce5e
savio <savio_ls@hotmail.com>
2025-04-08 06:32:28 -0300

    @Nested

--- src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
@@ -19,91 +20,103 @@ class ContaBancariaTest {
         conta = new ContaBancaria(new BigDecimal("100.00"));
     }
 
-    @Test
-    void deveCriarContaComSaldoValido() {
-        // Arrange
-        BigDecimal saldoInicial = new BigDecimal("50.00");
+    @Nested
+    class CriarConta{
 
-        // Act
-        ContaBancaria novaConta = new ContaBancaria(saldoInicial);
+        @Test
+        void deveCriarContaComSaldoValido() {
+            // Arrange
+            BigDecimal saldoInicial = new BigDecimal("50.00");
 
-        // Assert
-        assertEquals(saldoInicial, novaConta.saldo());
+            // Act
+            ContaBancaria novaConta = new ContaBancaria(saldoInicial);
+
+            // Assert
+            assertEquals(saldoInicial, novaConta.saldo());
+        }
+
+        @Test
+        void deveLancarExcecaoAoCriarContaComSaldoNulo() {
+            // Arrange
+            BigDecimal saldoInicial = null;
+
+            // Act & Assert
+            assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(saldoInicial));
+        }
     }
 
-    @Test
-    void deveLancarExcecaoAoCriarContaComSaldoNulo() {
-        // Arrange
-        BigDecimal saldoInicial = null;
+    @Nested
+    class Deposito {
+        @Test
+        void deveRealizarDepositoComValorValido() {
+            // Arrange
+            BigDecimal valorDeposito = new BigDecimal("25.00");
 
-        // Act & Assert
-        assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(saldoInicial));
+            // Act
+            conta.deposito(valorDeposito);
+
+            // Assert
+            assertEquals(new BigDecimal("125.00"), conta.saldo());
+        }
+
+        @ParameterizedTest
+        @NullSource
+        @ValueSource(strings = {"0", "-10.00", "-0.01"})
+        void deveLancarExcecaoAoDepositarValorInvalido(String valorStr) {
+            // Arrange
+            BigDecimal valor = valorStr == null ? null : new BigDecimal(valorStr);
+
+            // Act & Assert
+            assertThrows(IllegalArgumentException.class, () -> conta.deposito(valor));
+        }
     }
 
-    @Test
-    void deveRealizarDepositoComValorValido() {
-        // Arrange
-        BigDecimal valorDeposito = new BigDecimal("25.00");
 
-        // Act
-        conta.deposito(valorDeposito);
+    @Nested
+    class Saque{
+        @Test
+        void deveRealizarSaqueComValorValido() {
+            // Arrange
+            BigDecimal valorSaque = new BigDecimal("40.00");
 
-        // Assert
-        assertEquals(new BigDecimal("125.00"), conta.saldo());
+            // Act
+            conta.saque(valorSaque);
+
+            // Assert
+            assertEquals(new BigDecimal("60.00"), conta.saldo());
+        }
+
+        @Test
+        void deveLancarExcecaoAoSacarValorMaiorQueSaldo() {
+            // Arrange
+            BigDecimal valorSaque = new BigDecimal("150.00");
+
+            // Act & Assert
+            assertThrows(RuntimeException.class, () -> conta.saque(valorSaque));
+        }
+
+        @ParameterizedTest
+        @NullSource
+        @ValueSource(strings = {"0", "-20.00", "-0.01"})
+        void deveLancarExcecaoAoSacarValorInvalido(String valorStr) {
+            // Arrange
+            BigDecimal valor = valorStr == null ? null : new BigDecimal(valorStr);
+
+            // Act & Assert
+            assertThrows(IllegalArgumentException.class, () -> conta.saque(valor));
+        }
+
+        @Test
+        void deveRetornarSaldoAtual() {
+            // Arrange
+            BigDecimal saldoEsperado = new BigDecimal("100.00");
+
+            // Act
+            BigDecimal saldoAtual = conta.saldo();
+
+            // Assert
+            assertEquals(saldoEsperado, saldoAtual);
+        }
     }
 
-    @ParameterizedTest
-    @NullSource
-    @ValueSource(strings = {"0", "-10.00", "-0.01"})
-    void deveLancarExcecaoAoDepositarValorInvalido(String valorStr) {
-        // Arrange
-        BigDecimal valor = valorStr == null ? null : new BigDecimal(valorStr);
-
-        // Act & Assert
-        assertThrows(IllegalArgumentException.class, () -> conta.deposito(valor));
-    }
-
-    @Test
-    void deveRealizarSaqueComValorValido() {
-        // Arrange
-        BigDecimal valorSaque = new BigDecimal("40.00");
-
-        // Act
-        conta.saque(valorSaque);
-
-        // Assert
-        assertEquals(new BigDecimal("60.00"), conta.saldo());
-    }
-
-    @Test
-    void deveLancarExcecaoAoSacarValorMaiorQueSaldo() {
-        // Arrange
-        BigDecimal valorSaque = new BigDecimal("150.00");
-
-        // Act & Assert
-        assertThrows(RuntimeException.class, () -> conta.saque(valorSaque));
-    }
-
-    @ParameterizedTest
-    @NullSource
-    @ValueSource(strings = {"0", "-20.00", "-0.01"})
-    void deveLancarExcecaoAoSacarValorInvalido(String valorStr) {
-        // Arrange
-        BigDecimal valor = valorStr == null ? null : new BigDecimal(valorStr);
-
-        // Act & Assert
-        assertThrows(IllegalArgumentException.class, () -> conta.saque(valor));
-    }
-
-    @Test
-    void deveRetornarSaldoAtual() {
-        // Arrange
-        BigDecimal saldoEsperado = new BigDecimal("100.00");
-
-        // Act
-        BigDecimal saldoAtual = conta.saldo();
-
-        // Assert
-        assertEquals(saldoEsperado, saldoAtual);
-    }
 }

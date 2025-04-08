53943b996a8764920b691da90efa6f2b2f1ae5b1
savio <savio_ls@hotmail.com>
2025-04-08 06:14:20 -0300

    Display name

--- src/test/java/com/algaworks/junit/utilidade/PessoaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/PessoaTest.java
@@ -1,13 +1,16 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class PessoaTest {
 
     @Test
-    void assercaoAgrupada() {
+    void deve_valida_nomes_agrupados() {
         Pessoa pessoa = new Pessoa("Alex", "Silva");
 
         assertAll("Asserções de pessoa",
--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,13 +1,15 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@DisplayName("Testes no utilitário de saudação")
 class SaudacaoUtilTest {
 
     @Test
+    @DisplayName("Deve saudar com bom dia")
     public void saudarBomDia(){
         String saudacao = SaudacaoUtil.saudar(9);
         assertEquals("Bom dia", saudacao,"Saudação incorreta");
@@ -15,6 +17,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve saudar com bom dia apartir das 5h")
     public void saudarBomDiaApartir5h(){
         String saudacao = SaudacaoUtil.saudar(5);
         assertEquals("Bom dia", saudacao,"Saudação incorreta");
@@ -23,6 +26,7 @@ class SaudacaoUtilTest {
 
 
     @Test
+    @DisplayName("Deve saudar com boa tarde")
     public void saudarBoatarde(){
         String saudacao = SaudacaoUtil.saudar(12);
         assertEquals("Boa tarde", saudacao,"Saudação incorreta");
@@ -30,6 +34,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve saudar com boa noite")
     public void saudarBoaNoite(){
         String saudacao = SaudacaoUtil.saudar(18);
         assertEquals("Boa noite", saudacao,"Saudação incorreta");
@@ -37,6 +42,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve saudar com boa noite as 4h")
     public void saudarBoaNoiteAs4h(){
         String saudacao = SaudacaoUtil.saudar(4);
         assertEquals("Boa noite", saudacao,"Saudação incorreta");
@@ -44,14 +50,11 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve lancar exception com hora invalida")
     public void deveLancarException(){
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> SaudacaoUtil.saudar(-10), "Deve lançar exception");
         assertEquals("Hora invalida", exception.getMessage());
     }
 
-    @Test
-    public void naoDeveLancarException(){
-        assertDoesNotThrow(() -> SaudacaoUtil.saudar(12), "Deve lançar exception");
-    }
 }
